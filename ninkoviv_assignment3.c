d3595d01bbbd72a55351d6bb725168540a7cc509
Vanja Ninkovic <vanja.ninkovic27@gmail.com>
2025-02-07 13:20:48 -0600

    Added the ability to read the current directory and scan each entry. Added conditionals so that only files that start with movies_ and end with .csv will be parsed for file size. Created variables to store largest file name and largest file size to compare with each entry.

--- ninkoviv_assignment3.c
+++ ninkoviv_assignment3.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
+// function that returns a string of the largest csv file name in the directory
+int find_largest() {
+    // variables to store the prefix and suffix we check before deciding to inspect file
+    char prefix[] = "movies_";
+    char suffix[] = ".csv";
 
+    // open the current directory
+    DIR* currDir = opendir(".");
+    struct dirent *entry;
+    struct stat dirStat;
+    // Variables to store the current largest file name and file size
+    char *largest_file_name = calloc(256, sizeof(char));
+    int large_file_size;
 
+    while((entry = readdir(currDir)) != NULL){
+        // checking if prefix is "movies_"
+        if(strncmp(entry->d_name, prefix, strlen(prefix)) != 0){
+            continue;
+        }
+        // check if the extension of the file is a .csv
+        char *extension = strchr(entry->d_name, '.');
+        if(strcmp(extension, suffix) != 0){
+            continue;
+        }
+        printf("%s\n", entry->d_name);
+        if(largest_file_name == ""){
+            printf("File name is empty!\n");
+        }
+        
+    }
+    closedir(currDir);
+    return 0;
+
+}
+// function that returns a string of the smallest csv file name in the directory
+char* find_smallest() {
+
+}
+// function that returns 1 or 0 depending on if the file exists within the directory or not
+int find_by_name() {
+
+}
 
 
 
@@ -12,6 +55,7 @@ int main() {
 
     int menu_choice; // var to store choice at the first menu 
     int second_choice; // var to store choice at the second menu
+    char* file_name; // var to store the file name to be processed
     while(1) {
         
         printf("1. Select file to process\n");
@@ -34,11 +78,13 @@ int main() {
 
                 // Begin to parse the largest file in the current directory
                 if(second_choice == 1) {
+                    find_largest();
                     printf("You picked the largest file!\n");
                     break;
                 }
                 // Begin to parse the smallest file in the current directory
                 else if(second_choice == 2){
+                    file_name = find_smallest();
                     printf("You picked the smallest file!\n");
                     break;
                 }
