27bd53ad217e3c291e367e65a18a3e469c0cbe4f
Vanja Ninkovic <vanja.ninkovic27@gmail.com>
2025-02-07 12:08:42 -0600

    Created sub menu after user decides to parse a file.

--- ninkoviv_assignment3.c
+++ ninkoviv_assignment3.c
@@ -10,7 +10,8 @@
 // Main function to run the entire program
 int main() {
 
-    int menu_choice;
+    int menu_choice; // var to store choice at the first menu 
+    int second_choice; // var to store choice at the second menu
     while(1) {
         
         printf("1. Select file to process\n");
@@ -20,8 +21,37 @@ int main() {
 
         // Conditional to begin the entire file parsing process
         if(menu_choice == 1){
-            printf("You selected to process a file!\n");
-            break;
+            
+            // loop to keep asking the user to enter an option in case they enter a file that
+            // does not actually exist. Each function upon success will break this loop.
+            while(1){
+                printf("\nWhich file you want to process?\n");
+                printf("Enter 1 to pick the largest file\n");
+                printf("Enter 2 to pick the smallest file\n");
+                printf("Enter 3 to specify the name of a file\n\n");
+                printf("Enter a choice from 1 to 3: ");
+                scanf("%d", &second_choice);
+
+                // Begin to parse the largest file in the current directory
+                if(second_choice == 1) {
+                    printf("You picked the largest file!\n");
+                    break;
+                }
+                // Begin to parse the smallest file in the current directory
+                else if(second_choice == 2){
+                    printf("You picked the smallest file!\n");
+                    break;
+                }
+                // Ask the user to enter a specific file name to parse
+                else if(second_choice == 3){
+                    printf("You chose to enter a specific file!\n");
+                    break;
+                }
+                // Prompt the user to choose again as their input was incorrect
+                else {
+                    printf("You entered an incorrect choice. Try again.\n");
+                }
+            }
         }
         // breaks the while loop and ends the software
         else if(menu_choice == 2){
